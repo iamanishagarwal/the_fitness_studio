6765b0844ac1290d4d83f5de74687b38d41b7054
Anish Agarwal <iamanishagarwal@gmail.com>
2018-03-01 05:42:08 +0530

    Added some functionality and responsiveness to the website.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $('document').ready(function(){
+
+  $('ul li a').click(function(){
+    $('ul li a').removeClass("active");
+    $(this).addClass("active");
+  });
+
   $(".button").hover(
     function(){
-      $(this).css({"border" : "solid 4px #B2EBF2", "background" : "#B2EBF2"});
+      $(this).css({"border" : "solid 4px rgba(103, 241, 200, 1)", "background" : "rgba(103, 241, 200, 1)"});
     },
     function(){
       $(this).css({"border" : "solid 4px black", "background" : "transparent"});
@@ -23,5 +29,11 @@ $('document').ready(function(){
     function(){
       $(this).css('background-image' , 'url("img/girl2.jpeg")');
   });
+
+      $('.background_light .header_dark_1').animate({
+        top : 0,
+        opacity : 0.95
+      },3000
+      );
 
 });
